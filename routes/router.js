74b3a6710c0232cfc52ca94e3a1deed8839d9dac
ramantehlan <ramantehlan@gmail.com>
2018-02-03 19:27:48 +0530

    Styles Added | Linkes added

--- routes/router.js
+++ routes/router.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Finojo' , css: ['index.css']});
 });
 
+/* GET Generate page. */
+router.get('/generate/:type', function(req, res, next){
+  res.render('generate', { type: req.params.type});
+});
+
+/* GET View page. */
+router.get('/view/:type', function(req, res, next){
+  res.render('view', { type: req.params.type});
+});
+
 module.exports = router;
