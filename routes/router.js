d8cf6abf66ddb692d54a0edf3bd7d111cf55058b
ramantehlan <ramantehlan@gmail.com>
2018-02-03 21:40:34 +0530

    Ipfs added

--- routes/router.js
+++ routes/router.js
@@ -8,12 +8,59 @@ router.get('/', function(req, res, next) {
 
 /* GET Generate page. */
 router.get('/generate/:type', function(req, res, next){
-  res.render('generate', { title: 'Generate', type: req.params.type , css :[]});
+
+  let type = req.params.type;
+  let items = [];
+  let generate = true;
+
+  if(type == "policy"){
+    items = ["Corporate Id", "User Id", "Policy Type", "Amount", "Other Details"];
+  }else if(type == "medical"){
+    items = ["Corporate Id", "User Id", "Patient type", "Days of stay", "Responsible Doctor", "Deseas", "Cause of Deases", "Medication" ,"Other Details"];
+  }else if (type == "contract") {
+    items = ["Corporate Id", "User Id", "Contract Type", "Validation period", "Content"];
+  }else{
+    generate = false;
+  }
+
+  res.render('generate', {
+                title: type + ' Generator',
+                type: req.params.type ,
+                css :[],
+                items : items,
+                generate: generate
+            });
 });
 
 /* GET View page. */
 router.get('/view/:type', function(req, res, next){
   res.render('view', { title: 'View', type: req.params.type , css: []});
+});
+
+/* GET View page. */
+router.post('/ipfs/store/:type', function(req, res, next){
+  let type = req.params.type;
+
+  if(type == "policy"){
+    let cId = req.body.id0;
+    let uId = req.body.id1;
+    let pType = req.body.id2;
+    let amount = req.body.id3;
+    let other = req.body.id4;
+
+      res.render('ipfsStore', {
+        title: 'Storing in IPFS',
+        cId: cId,
+        uId: uId,
+        pType: pType,
+        amount: amount,
+        other:other, 
+        css: []});
+  }else{
+      res.send('respond with a resource');
+  }
+
+
 });
 
 module.exports = router;
