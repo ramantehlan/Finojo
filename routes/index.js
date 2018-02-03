fdc450a46f4993f3d00592a4cf98ceb185f36e4b
ramantehlan <ramantehlan@gmail.com>
2018-02-03 17:05:12 +0530

    Added nodemon package

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Finojo' });
+  res.render('index', { title: 'Finojo - Decentralized safe recorder' });
 });
 
 module.exports = router;
